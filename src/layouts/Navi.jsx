6360c63bf993bc9124b338f1c81d0dd08807157b
tnhnyldrm <46500254+tnhnyldrm@users.noreply.github.com>
2024-01-01 18:23:04 +0300

    signIn signOut added (video 12)

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CartSummary from './CartSummary'
 import { Button, Container, Dropdown, Menu } from 'semantic-ui-react'
+import SignedOut from './SignedOut'
+import SignedIn from './SignedIn'
 
 export default function Navi() {
+    const [isAuthenticated, setIsAuthenticated] = useState(true)
+
+    function handleSignedOut(params) {
+        setIsAuthenticated(false)
+    }
+
+    function handleSignedIn() {
+        setIsAuthenticated(true)
+    }
+
     return (
         <div>
             <Menu inverted fixed='top'>
                 <Container>
-                    <Menu.Item
-                        name='home'
-                    />
-                    <Menu.Item
-                        name='messages'
-                    />
+                    <Menu.Item name='home' />
+                    <Menu.Item name='messages' />
 
                     <Menu.Menu position='right'>
-                        <CartSummary/>
-                        <Menu.Item>
-                            <Button primary>Sign Up</Button>
-                        </Menu.Item>
+                        <CartSummary />
+                        {isAuthenticated?<SignedIn signOut={handleSignedOut} bisey="1"/>:<SignedOut signIn={handleSignedIn}/>}
                     </Menu.Menu>
                 </Container>
             </Menu>
---
+++ src/layouts/SignedIn.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Dropdown, Image, Menu } from 'semantic-ui-react'
+
+export default function SignedIn({signOut}) {
+    return (
+        <div>
+            <Menu.Item>
+                <Image avatar spaced="right" src="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png" />
+                <Dropdown pointing="top left" text='Tuna'>
+                    <Dropdown.Menu>
+                        <Dropdown.Item text="Bilgilerim" icon="info"/>
+                        <Dropdown.Item onClick={signOut} text="Çıkış Yap" icon="sign-out"/>
+                    </Dropdown.Menu>
+                </Dropdown>
+
+            </Menu.Item>
+        </div>
+    )
+}
